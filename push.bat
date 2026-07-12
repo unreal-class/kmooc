67db81fb7736a6b8486ec06915ca343db56c6eac
daehyunlee-interual <daehyun.lee@interual.com>
2026-07-12 18:41:54 +0900

    push 할 때 커밋 메시지 받도록 수정.

--- push.bat
+++ push.bat
@@ -1,5 +1,8 @@
+@echo off
+set /p commit_msg="커밋 메시지를 입력하세요: "
+
 git.exe add .
-git.exe commit -m 'sync'
+git.exe commit -m "%commit_msg%"
 git.exe push --progress "origin" --all
 git.exe push --progress "origin" --tags
 pause
